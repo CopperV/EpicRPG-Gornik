9e229a94ea95af32111ff93236ee321c3e2e0e7c
CopperV <vark123@interia.pl>
2023-08-22 23:34:13 +0200

    fix

---
+++ src/main/java/me/Vark123/EpicRPGGornik/CataclysmControllers/Listeners/CataclysmMineResetListener.java
@@ -0,0 +1,22 @@
+package me.Vark123.EpicRPGGornik.CataclysmControllers.Listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+import me.Vark123.EpicRPGGornik.PlayerSystem.PlayerManager;
+import me.nikl.calendarevents.CalendarEvent;
+
+public class CataclysmMineResetListener implements Listener {
+	
+	@EventHandler
+	public void onDate(CalendarEvent e) {
+		if(!e.getLabels().contains("reset_mines")) 
+			return;
+		
+		PlayerManager.get().getMiners().values().forEach(miner -> {
+			miner.getCatMiner().setRemainTime(miner.getCatMiner().getMaxTime());
+			miner.getCatMiner().setLastResetTime(Long.parseLong(e.getTime()));
+		});
+	}
+
+}
